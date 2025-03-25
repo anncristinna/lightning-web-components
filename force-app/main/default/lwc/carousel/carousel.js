97cd3c1c30bb91a608a1eaf41a3ecefca6d914ef
Anna Cristina <annacristinaferreiraalves@gmail.com>
2025-03-24 21:08:03 -0300

    relacionamento entre componentes

--- force-app/main/default/lwc/carousel/carousel.js
+++ force-app/main/default/lwc/carousel/carousel.js
@@ -1,55 +1,9 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
 export default class Carousel extends LightningElement {
+    @api dados;
     pagina = 0;
-    
-    dados = [
-        {
-          id: 1,
-          empresa: "Tech Solutions",
-          produtos: [
-            { id: 101, nome: "Notebook Pro" },
-            { id: 102, nome: "Smartphone X" }
-          ],
-          distribuidoras: [
-            { id: 201, nome: "Distribuidora Global" }
-          ],
-          lojas: [
-            { id: 301, nome: "Loja Centro" },
-            { id: 302, nome: "Loja Norte" }
-          ]
-        },
-        {
-          id: 2,
-          empresa: "InovaTech",
-          produtos: [
-            { id: 103, nome: "Tablet Max" },
-            { id: 104, nome: "Fone Bluetooth" }
-          ],
-          distribuidoras: [
-            { id: 202, nome: "Distribuidora Express" }
-          ],
-          lojas: [
-            { id: 303, nome: "Loja Sul" },
-            { id: 304, nome: "Loja Oeste" }
-          ]
-        },
-        {
-          id: 3,
-          empresa: "FutureTech",
-          produtos: [
-            { id: 105, nome: "Smartwatch Ultra" },
-            { id: 106, nome: "Câmera 4K" }
-          ],
-          distribuidoras: [
-            { id: 203, nome: "Distribuidora Rápida" }
-          ],
-          lojas: [
-            { id: 305, nome: "Loja Leste" },
-            { id: 306, nome: "Loja Central" }
-          ]
-        }
-      ];
+    paginaSelecionada;
 
       get botaoRetorno () {
         return this.pagina === 0;
@@ -63,6 +17,10 @@ export default class Carousel extends LightningElement {
         return `
           transform: translateX(-${100 * this.pagina}%);
         `
+      }
+
+      get slideJs() {
+        return this.pagina === this.paginaSelecionada ? 'slide slideSelecionado' : 'slide';
       }
 
       previousSlide() {
---
+++ force-app/main/default/lwc/componentePai/componentePai.js
@@ -0,0 +1,57 @@
+import { LightningElement } from 'lwc';
+
+export default class ComponentePai extends LightningElement {
+
+    dados = [
+        {
+          id: 1,
+          empresa: "Tech Solutions",
+          produtos: [
+            { id: 101, nome: "Notebook Pro" },
+            { id: 102, nome: "Smartphone X" }
+          ],
+          distribuidoras: [
+            { id: 201, nome: "Distribuidora Global" }
+          ],
+          lojas: [
+            { id: 301, nome: "Loja Centro" },
+            { id: 302, nome: "Loja Norte" }
+          ]
+        },
+        {
+          id: 2,
+          empresa: "InovaTech",
+          produtos: [
+            { id: 103, nome: "Tablet Max" },
+            { id: 104, nome: "Fone Bluetooth" }
+          ],
+          distribuidoras: [
+            { id: 202, nome: "Distribuidora Express" }
+          ],
+          lojas: [
+            { id: 303, nome: "Loja Sul" },
+            { id: 304, nome: "Loja Oeste" }
+          ]
+        },
+        {
+          id: 3,
+          empresa: "FutureTech",
+          produtos: [
+            { id: 105, nome: "Smartwatch Ultra" },
+            { id: 106, nome: "Câmera 4K" }
+          ],
+          distribuidoras: [
+            { id: 203, nome: "Distribuidora Rápida" }
+          ],
+          lojas: [
+            { id: 305, nome: "Loja Leste" },
+            { id: 306, nome: "Loja Central" }
+          ]
+        }
+      ];
+
+      handleSelecionarPai() {
+        this.paginaSelecionada = this.pagina;
+        console.log(JSON.stringify(this.dados[this.pagina]))
+      }
+}
