a2a32afb46033da1b90bcf19a6d1a08dafe370d5
Anna Cristina <annacristinaferreiraalves@gmail.com>
2025-03-29 20:51:07 -0300

    apex class

--- force-app/main/default/lwc/componentePai/componentePai.js
+++ force-app/main/default/lwc/componentePai/componentePai.js
@@ -1,54 +1,32 @@
 import { LightningElement } from 'lwc';
+import getAccount from '@salesforce/apex/ComponentePaiController.getAccounts';
+import getDadosEmpresa from '@salesforce/apex/ComponentePaiController.getDadosEmpresa';
 
 export default class ComponentePai extends LightningElement {
 
-    dadosPai = [
-        {
-          id: 1,
-          empresa: "Tech Solutions",
-          produtos: [
-            { id: 101, nome: "Notebook Pro" },
-            { id: 102, nome: "Smartphone X" }
-          ],
-          distribuidoras: [
-            { id: 201, nome: "Distribuidora Global" }
-          ],
-          lojas: [
-            { id: 301, nome: "Loja Centro" },
-            { id: 302, nome: "Loja Norte" }
-          ]
-        },
-        {
-          id: 2,
-          empresa: "InovaTech",
-          produtos: [
-            { id: 103, nome: "Tablet Max" },
-            { id: 104, nome: "Fone Bluetooth" }
-          ],
-          distribuidoras: [
-            { id: 202, nome: "Distribuidora Express" }
-          ],
-          lojas: [
-            { id: 303, nome: "Loja Sul" },
-            { id: 304, nome: "Loja Oeste" }
-          ]
-        },
-        {
-          id: 3,
-          empresa: "FutureTech",
-          produtos: [
-            { id: 105, nome: "Smartwatch Ultra" },
-            { id: 106, nome: "Câmera 4K" }
-          ],
-          distribuidoras: [
-            { id: 203, nome: "Distribuidora Rápida" }
-          ],
-          lojas: [
-            { id: 305, nome: "Loja Leste" },
-            { id: 306, nome: "Loja Central" }
-          ]
+    dadosPai;
+
+      connectedCallback() {
+        this.pegarValores();
+        this.pegarDadosEmpresa();
+      }
+
+      async pegarDadosEmpresa() {
+        try {
+          this.dadosPai = await getDadosEmpresa();
+        } catch(err) {
+          console.log(err)
         }
-      ];
+      }
+
+      async pegarValores() {
+        try {
+          let contas = await getAccount();
+        } catch(err) {
+          console.log(err)
+        }
+        
+      }
 
        handleSelecionarPai(event) {
         console.log(JSON.stringify(event.detail.dado))
